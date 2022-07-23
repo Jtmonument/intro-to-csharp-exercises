dff6fc245e33c180c12069ed95aacd8808e215e7
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 00:26:13 -0500

    Code Exercise 1.

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Exercise1
 {
@@ -6,7 +7,17 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Write("Enter a number between 1 and 10: ");
+            int number = Convert.ToInt32(ReadLine());
+            WriteLine(number);
+            if (number >= 1 && number <= 10)
+            {
+                WriteLine("\nValid");
+            }
+            else
+            {
+                WriteLine("\nInvalid");
+            }
         }
     }
 }
