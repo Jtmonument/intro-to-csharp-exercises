f3b0af43f45a99a3a1598e6bdbb19a7971dba8bc
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 02:37:42 -0500

    Code Exercise 5.

---
+++ Exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers: ");
+            string input = Console.ReadLine();
+            int maxNumber = int.MinValue;
+            foreach (string character in input.Replace(" ", "").Trim().Split(","))
+            {
+                int numParsed;
+                if (int.TryParse(character, out numParsed) && numParsed > maxNumber)
+                {
+                    maxNumber = numParsed;
+                }
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
