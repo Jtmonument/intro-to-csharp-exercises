372a9a46ff4fe8ab8fbd14cd8345103942c201bd
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 11:54:21 -0500

    Code Exercise 2.

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -6,12 +6,23 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            char[] name = Console.ReadLine().ToCharArray();
-            Array.Reverse(name);
-            foreach (char ch in name)
+            Console.Write("Enter numbers: ");
+            string[] input = Console.ReadLine().Split('-');
+            if (input.Length == 0)
             {
-                Console.WriteLine(ch);
+                Environment.Exit(0);
+            }
+
+            var numbers = new int[input.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = Convert.ToInt32(input[i]);
+                if (Array.Exists(numbers, num => num == number))
+                {
+                    Console.WriteLine("Duplicate");
+                    break;
+                }
+                numbers[i] = number;
             }
         }
     }
