ed9d24c7f16997a0a66dcb9ae976319aaa9bd351
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-23 00:33:40 -0500

    Code Exercise 2.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Write("Enter second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            WriteLine(firstNumber > secondNumber ? firstNumber : secondNumber);
+        }
+    }
+}
